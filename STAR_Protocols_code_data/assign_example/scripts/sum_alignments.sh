08dd15304b869da5a9932ead750f6a89dc50e37c
Xiang Zhang <630250299@qq.com>
2024-11-18 00:00:10 -0600

    Update sum_alignments.sh

--- STAR_Protocols_code_data/assign_example/scripts/sum_alignments.sh
+++ STAR_Protocols_code_data/assign_example/scripts/sum_alignments.sh
@@ -4,12 +4,15 @@
 ODIR=$(date +'../output/MappingOutput-%F')
 
 ls -l ${ODIR}/*.map > /dev/null 2>&1    
+
+# Check if .map files exist
 if [ "$?" = "0" ]; then
    cd ${ODIR}                          
+   # runs a Java program named summarizeAlignments.jar with a maximum heap size of 4096 MB and two arguments (reference txt and .map file)
    java -Xmx4096M -jar ../../Library/summarizeAlignments.jar ../../Library/Candida_barcode_V16_final_annotation_GRACEV1V2.txt `find ./ -name '*.map' -print | tr '\n' ','`         
 
    if [ "$?" > "0" ]; then
-      echo "There was an error merging files!"
+      echo "Program completes, or there could be an error merging files."
       cd -
       exit 1
    fi
